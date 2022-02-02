6ff71d80feef40ba5d2e549f9caa5e17cba8c5a0
Yuguo Zheng <zhenghugo515@gmail.com>
2022-02-01 20:46:12 -0500

    Sync model and API

--- src/main/java/model/appointment/Appointment.java
+++ src/main/java/model/appointment/Appointment.java
@@ -1,5 +1,30 @@
 package model.appointment;
 
+import java.util.ArrayList;
+import model.role.PatientId;
+
 public class Appointment {
-  private AppointmentId appointmentId;
+  private final AppointmentId appointmentId;
+  private final int capacity;
+  private final ArrayList<PatientId> patientIds;
+
+  public Appointment(AppointmentId appointmentId, int capacity ) {
+    this.appointmentId = appointmentId;
+    this.capacity = capacity;
+    this.patientIds = new ArrayList<>();
+  }
+
+  public AppointmentId getAppointmentId() {
+    return this.appointmentId;
+  }
+
+  public int getRemainingCapacity() {
+    return this.capacity - patientIds.size();
+  }
+
+  public void addPatient(PatientId id) {
+    if (patientIds.size() < capacity) {
+      this.patientIds.add(id);
+    }
+  }
 }
